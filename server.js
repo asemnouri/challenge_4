8e602a2d4ab91648d6ac3ec06301edc9ba2e5eda
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-11-05 11:41:12 +0200

    connected to server

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
+const express = require('express');
+
+let app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())
 
 
+let port = 3000;
+
+
+
+app.get("/app", (req, res) => {
+    res.sendFile(__dirname + '/./client/dist/bundle.js')
+})
+app.listen(port, () => {
+    console.log(`listening `);
+});
